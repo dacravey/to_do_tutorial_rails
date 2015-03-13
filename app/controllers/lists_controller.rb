ffdd002c4fd02c1d84e785c1e9994faee0dc5f5b
Debbie Cravey <cinemaestro@yahoo.com>
2015-03-13 15:53:31 -0700

    refactor to elimitate unnecessary render commands and add homepage button to application template

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,10 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
-    render :index
   end
 
   def new
     @list = List.new
-    render :new
   end
 
   def create
@@ -21,12 +19,10 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    render :show
   end
 
   def edit
     @list = List.find(params[:id])
-    render :edit
   end
 
   def update
