6dc3eced7d46f474b2a4e96140ec79b564942145
Debbie Cravey <cinemaestro@yahoo.com>
2015-03-13 14:28:39 -0700

    add ability to edit lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,8 +4,38 @@ class ListsController < ApplicationController
     render :index
   end
 
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    @list = List.new(params[:list])
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
   def show
     @list = List.find(params[:id])
     render :show
   end
+
+  def edit
+    @list = List.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(params[:list])
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+
 end
