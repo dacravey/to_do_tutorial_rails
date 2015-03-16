55d6ed97b7207501a829d95d3635ec27b803b5de
Debbie Cravey and Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-03-16 10:34:02 -0700

    add done property and test

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ActiveRecord::Base
   belongs_to :list
 
   validates :description, :presence => true
+
+  after_create do
+    self.done = false
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,4 +3,12 @@ require 'rails_helper'
 describe Task do
   it { should validate_presence_of :description }
   it { should belong_to :list}
+
+  describe("#done") do
+    it("will return done as false when initizialzed") do
+      test_task = Task.create(description: "Clean up")
+      expect(test_task.done).to(eq(false))
+    end
+  end
+
 end
